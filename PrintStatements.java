68153ae8a03e5015516261b145332a317f02b760
SdetEmin <createnautomate@gmail.com>
2020-07-16 14:19:05 -0500

    added PrintStatements program

---
+++ PrintStatements.java
@@ -0,0 +1,21 @@
+package intro;
+
+public class PrintStatements {
+
+    public static void main(String[] args) {
+
+//        System.out.print( "Welcome to " );
+//        System.out.println( "Java Programming!" );
+
+        System.out.println( "Welcome\nto\nJava\nProgramming!" );
+
+        System.out.println( "\"in quotes\"" );
+
+        System.out.println("=====================");
+
+        System.out.printf( "%s\n%s\n",
+                "Welcome to", "Java Programming!" );
+    }
+
+
+}
